97467eb8e530a7fcfee65573fa49f210e25926a4
NIASKA <mkman751@yahoo.com>
2021-08-03 18:17:41 -0500

    test

--- server/server.js
+++ server/server.js
@@ -1,35 +1,38 @@
-const express = require('express')
-const {ApolloServer} = require('apollo-server-express')
-const path = require('path')
-const db = require('./config/connection')
-const {authMiddleware} = require('./utils/auth')
-const {typeDefs, resolvers} = require('./schemas')
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
-const PORT = process.env.PORT || 3001
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware
-})
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
-server.applyMiddleware({app})
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+server.applyMiddleware({ app });
 
-app.use('/images', express.static(path.join(__dirname, '../client/images')))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-if(process.env.NODE_ENV === 'production')  {
-    app.use(express.static(path.join(__dirname, '../client.build')))
-}  
+// Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
-})
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}`)
-        console.log(`Use GRAPHQL at ${PORT}${server.graphqlPath}`)
-    })
-})
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
