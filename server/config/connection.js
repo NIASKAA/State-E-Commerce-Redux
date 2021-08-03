71e259012b945da10aa0d53ccf58d7ad71f88769
NIASKA <mkman751@yahoo.com>
2021-08-03 18:07:54 -0500

    test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_KEY || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGOKEY || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
