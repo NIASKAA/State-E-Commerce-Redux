3044aca16d1d5872897fac686edd87bf970d7840
NIASKA <mkman751@yahoo.com>
2021-08-03 11:06:07 -0500

    set up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,50 @@
 import React from 'react'
 import {BrowserRouter, Router, Route, Switch} from 'react-router-dom'
-import {ApolloProvider, ApolloClient} from '@apollo/client';
+import {ApolloProvider, ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client'
+import {setContext} from '@apollo/client/client/link/context'
 import {Home, Detail, NoMatch, Login, Signup, OrderHistory, Success} from './Pages'
 import {Nav} from './Components/Nav';
-
 import './App.css';
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+})
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
 function App() {
   return (
-    <>
-      
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/success" component={Success} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
